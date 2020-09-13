17ac207a72181cb95831aa52366f28bb5cf10fcc
Jonathan Joly <jolyjonathan89@gmail.com>
2020-09-13 14:54:49 +0100

    Update Observer

--- Observer/Subject.js
+++ Observer/Subject.js
@@ -1,18 +1,18 @@
 function Subject() {
-    this.observers = new ObserverList();
+  this.observers = new Observers();
 }
 
 Subject.prototype.addObserver = function (observer) {
-    this.observers.add(observer);
+  this.observers.add(observer);
 };
 
 Subject.prototype.removeObserver = function (observer) {
-    this.observers.removeAt(this.observers.indexOf(observer, 0));
+  this.observers.removeAt(this.observers.indexOf(observer, 0));
 };
 
 Subject.prototype.notify = function (context) {
-    var observerCount = this.observers.count();
-    for (var i = 0; i < observerCount; i++) {
-        this.observers.get(i).update(context);
-    }
+  var observerCount = this.observers.count();
+  for (var i = 0; i < observerCount; i++) {
+    this.observers.get(i).update(context);
+  }
 };
